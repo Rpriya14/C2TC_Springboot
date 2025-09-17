9660a10bdc9035ca4dd9c385405e5f69c88d3fb3
Hp <Hp@LAPTOP-07P0GB5R>
2025-09-17 19:16:10 +0530

    Commit

---
+++ customerservice/src/main/java/com/tnsif/customerservice/controller/CustomerController.java
@@ -0,0 +1,57 @@
+package com.tnsif.customerservice.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.tnsif.customerservice.entity.Customer;
+import com.tnsif.customerservice.service.CustomerService;
+
+import jakarta.persistence.NoResultException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    @Autowired
+    CustomerService c;
+
+    @GetMapping("/customerservice")
+    public List<Customer> list() {
+        return c.listAll();
+    }
+
+    @PostMapping("/customerservice")
+    public void add(@RequestBody Customer cl) {
+        c.save(cl);
+    }
+
+    @GetMapping("/customerservice/{id}")
+    public ResponseEntity<Customer> get(@PathVariable Integer id) {
+        try {
+            Customer c2 = c.get(id);
+            return new ResponseEntity<Customer>(c2, HttpStatus.OK);
+        } catch (NoResultException e) {
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/customerservice/{id}")
+    public void delete(@PathVariable Integer id) {
+        c.delete(id);
+    }
+
+    @PutMapping("/customerservice/{id}")
+    public ResponseEntity<Customer> update(@PathVariable Integer id, @RequestBody Customer update_c) {
+        try {
+            Customer exist_c = c.get(id);
+            exist_c.setC_name(update_c.getC_name());
+            exist_c.setAddress(update_c.getAddress());
+            c.update(exist_c);
+            return new ResponseEntity<Customer>(exist_c, HttpStatus.OK);
+        } catch (NoResultException e) {
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
